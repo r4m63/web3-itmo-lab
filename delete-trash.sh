c3f82352182f4ae089392ce9a03b7d178fe4e6f5
ramil <rm.tj.777@gmail.com>
2025-04-18 21:26:52 +0300

    autotools total fixes

--- delete-trash.sh
+++ delete-trash.sh
@@ -1,1 +1,1 @@
-rm -rf configure config.status config.log aclocal.m4 missing install-sh ./autom4te.cache/
+rm -rf configure config.status config.log aclocal.m4 missing install-sh ./autom4te.cache/ Makefile Makefile.in
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+rm -rf configure config.status config.log aclocal.m4 missing install-sh ./autom4te.cache/
+
 aclocal
 autoconf
 automake --add-missing
